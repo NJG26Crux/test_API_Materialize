1c1dd0035e7168c3efce20a0587582f52ca13e01
Jeff Mobley <njg26crux@gmail.com>
2017-07-10 18:25:38 -0700

    another commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
       $li.append($liHdr);
 
       const $liBdy = $('<div class="collapsible-body">');
-      const $liBdyText = $('<span>').text(beers[id].tagline);
+      // const $liBdyText = $('<span>').text(beers[id].tagline);
 
       const $liRow = $("<div>").attr("class", "row");
       $liBdy.append($liRow);
@@ -172,7 +172,132 @@ $(document).ready(function(){
     // another click event for recipies ".recipe_btn"
     $(".recipe_btn").click(function() {
       $("#Recipe").empty();
+      $("#listings").slideUp("slow");
       const arrNum = $(this).attr("beerId");
+
+      const $divS10 = $("<div>").attr("class", "col s10");
+      const $divTitle = $("<div>").text(beers[arrNum].name + " ( " + beers[arrNum].tagline + " )").attr("width", "100%").attr("class", " btmMarg");
+      $divS10.append($divTitle);
+      const $hr = $('<hr>');
+      $divS10.append($hr);
+      const $divDisc = $('<div>').text(beers[arrNum].description);
+      $divS10.append($divDisc);
+
+
+
+
+      // start of table ****************************************
+      const $table = $("<table>");
+      const $trow1 = $("<tr>");
+
+      const abvText = ("ABV: " + beers[arrNum].abv);
+      const $abvModal1 = $("<a>").text(abvText).attr({class: "modal-close", href: "#modal1"});
+      var $tr1cell1 = $("<td>");
+      $tr1cell1.append($abvModal1);
+      $trow1.append($tr1cell1);
+
+      const ibuText = ("IBU: " + beers[arrNum].ibu);
+      const $ibuModal2 = $("<a>").text(ibuText).attr({class: "modal-close", href: "#modal2"});
+      const $tr1cell2 = $("<td>");
+      $tr1cell2.append($ibuModal2);
+      $trow1.append($tr1cell2);
+
+      const phText = ("PH: " + beers[arrNum].ph);
+      const $phModal5 = $("<a>").text(phText).attr({class: "modal-close", href: "#modal5"});
+      const $tr1cell3 = $("<td>");
+      $tr1cell3.append($phModal5);
+      $trow1.append($tr1cell3);
+      $table.append($trow1);
+
+      const $trow2 = $("<tr>");
+
+      const ebcText = ("EBC: " + beers[arrNum].ebc);
+      const $ebcModal3 = $("<a>").text(ebcText).attr({class: "modal-close", href: "#modal3"});
+      const $tr2cell1 = $("<td>");
+      $tr2cell1.append($ebcModal3);
+      $trow2.append($tr2cell1);
+
+      const srmText = ("SRM: " + beers[arrNum].srm);
+      const $srmModal4 = $("<a>").text(srmText).attr({class: "modal-close", href: "#modal4"});
+      const $tr2cell2 = $("<td>");
+      $tr2cell2.append($srmModal4);
+      $trow2.append($tr2cell2);
+
+      const $tr2cell3 = $("<td>");
+      const $colorText = $("<span>").text("Color: ");
+      $tr2cell3.append($colorText);
+        var ebcColor = '';
+        beers[arrNum].ebc = Number(beers[arrNum].ebc);
+        if ( beers[arrNum].ebc < 6) {
+          ebcColor = ('colorBtn5');
+        } else if ( beers[arrNum].ebc < 8) {
+            ebcColor = ('colorBtn7');
+        } else if ( beers[arrNum].ebc < 12) {
+            ebcColor = ('colorBtn11');
+        } else if ( beers[arrNum].ebc < 16) {
+            ebcColor = ('colorBtn15');
+        } else if ( beers[arrNum].ebc < 20) {
+            ebcColor = ('colorBtn19');
+        } else if ( beers[arrNum].ebc < 26) {
+            ebcColor = ('colorBtn25');
+        } else if ( beers[arrNum].ebc < 33) {
+            ebcColor = ('colorBtn32');
+        } else if ( beers[arrNum].ebc < 39) {
+            ebcColor = ('colorBtn38');
+        } else if ( beers[arrNum].ebc < 47) {
+            ebcColor = ('colorBtn46');
+        } else if ( beers[arrNum].ebc < 57) {
+            ebcColor = ('colorBtn56');
+        } else if ( beers[arrNum].ebc < 69) {
+            ebcColor = ('colorBtn68');
+        } else if ( beers[arrNum].ebc < 79) {
+            ebcColor = ('colorBtn78');
+        } else if ( beers[arrNum].ebc === 79) {
+            ebcColor = ('colorBtn79');
+        } else if ( beers[arrNum].ebc > 79) {
+            ebcColor = ('colorBtn79');
+        }
+      const $colorBtn = $("<a>").attr("class", "btn " + ebcColor);
+
+      $tr2cell3.append($colorBtn);
+
+      $trow2.append($tr2cell3);
+      $table.append($trow2);
+
+      const $trow3 = $("<tr>");
+
+      const attenlevelText = ("Attenuation level: " + beers[arrNum].attenuation_level);
+      const $attenlevelModal6 = $("<a>").text(attenlevelText).attr({class: "modal-close", href: "#modal6"});
+      const $tr3cell1 = $("<td>").attr("colspan", "2");
+      $tr3cell1.append($attenlevelModal6);
+      $trow3.append($tr3cell1);
+
+      const $tr3cell3 = $("<td>");
+      // const $recipeBtn = $("<button>").text("Recipe").attr({"beerId": id, class: "btn btn-small recipe_btn"});
+      // const $foodBtn = $("<button>").text("Food").attr({"beerId": id, class: "btn btn-small food_btn"});
+      // $tr3cell3.append($recipeBtn);
+      // $tr3cell3.append($foodBtn);
+      // $trow3.append($tr3cell3);
+
+      // const foodPairings = (beers[arrNum].food_pairing);
+      // var newFoodPairings =  foodPairings.join(', ');
+      // const $foodPair = $('<div>').text("Food Pairings: " + newFoodPairings);
+
+      $table.append($trow3);
+      $divS10.append($table);
+      // $liBdyDisc.append($foodPair);
+      // const $hr = $('<hr>');
+      $divS10.append($hr);
+
+      $("#Recipe").append($divS10);
+      // end of table ******************************************
+
+
+
+      const $divS2 = $("<div>").attr("class", "col s2");
+      const $liImg = $("<img>").attr({"src": beers[arrNum].image_url, "height": "200px", "width": "auto", "align": "center"});
+      $divS2.append($liImg);
+      $("#Recipe").append($divS2);
 
       const $divS12 = $("<div>").attr("class", "col s12");
       const boilVolume = beers[arrNum].boil_volume;
